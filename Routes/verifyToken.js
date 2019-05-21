86e50282825669f875ce50ae4e8654d3df6742a9
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-05-21 15:59:18 -0400

    verifyToken modified.  forgot to add a next()

--- Routes/verifyToken.js
+++ Routes/verifyToken.js
@@ -8,6 +8,8 @@ module.exports = function( req, res, next )
     try {
         const verified = jwt.verify( token, process.env.Token_SECRET )
         req.user = verified
+        //push through to the next screen
+        next()
 
     } catch ( err ) {
         res.status(400).send('invalid token')
