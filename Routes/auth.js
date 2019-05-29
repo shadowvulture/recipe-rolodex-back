00ffb1a629bf03fa1c0d85311ec7d69edbed7ed4
Nathan Rotach <Nathan.Rotach@gmail.com>
2019-05-29 09:32:36 -0400

    attempt #alot

--- Routes/auth.js
+++ Routes/auth.js
@@ -30,7 +30,10 @@ router.post( '/register', async ( req, res ) =>
         const savedUser = await user.save()
         res.send( ( { savedUser } ) )
         console.log(savedUser)
-        res.send({ user: user._id})
+        res.send( { user: user._id } )
+            //  create and assign a token
+    const token = jwt.sign({ _id: savedUser._id }, process.env.TOKEN_SECRET )
+    res.header('auth-token', token).send(token)
     } catch ( err ) {
         res.status(400).send(err)
     }
